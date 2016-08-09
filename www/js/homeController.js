e0f50c1e73f3ee5565ed1944c8ee74ddd569faf0
Bhupinder Garg <bhupinder.garg.js@snapdeal.com>
2016-08-10 00:01:52 +0530

    reload hunt on pull-to-refresh.

--- www/js/homeController.js
+++ www/js/homeController.js
@@ -1,44 +1,56 @@
 
 module.controller('homeController', function($scope, $http, $localStorage, $sessionStorage) {
 
-  var url = "https://api.producthunt.com/v1/posts";
-  topicFilter = false;
-  // 208 angel investing
-  if (topicFilter) {
-      url = url + "/all?search[topic]=" + 208; // TODO paging.
-  }
-  else {
-      var daysAgo = Math.floor(Math.random() * 251);
-      url = url + "?days_ago=" + daysAgo;
-  }
-  var headerOptions = {
-    headers: {"Authorization":"Bearer fbf62059f1556488e5354cd3892095e35ac776e93769efc2a446df35db8b7e6c"}
-  };
+  // $scope.$storage.huntq ||= []
+  $scope.refreshHunt = function($done) {
+      console.log("Refreshing hunt...")
+      var url = "https://api.producthunt.com/v1/posts";
+      topicFilter = false;
+      // 208 angel investing
+      if (topicFilter) {
+          url = url + "/all?search[topic]=" + 208; // TODO paging.
+      }
+      else {
+          var daysAgo = Math.floor(Math.random() * 251);
+          url = url + "?days_ago=" + daysAgo;
+      }
+      var headerOptions = {
+        headers: {"Authorization":"Bearer fbf62059f1556488e5354cd3892095e35ac776e93769efc2a446df35db8b7e6c"}
+      };
 
-  $http.get(url, headerOptions).then(
-    function(response) {
-      console.log("Received success from url " + url);
-        // First function handles success
-        body = response.data['posts'];
-        var ret = body[Math.floor(Math.random() * body.length)];
-        $scope.randomhunt = ret;
-        var hasAudio = ret.thumbnail.media_type == 'audio';
-        if (hasAudio) {
-            var audioUrl = ret.thumbnail.metadata.url;
-            console.log("Audio meta detected " + audioUrl);
-            if (audioUrl.startsWith("http:")) {
-                audioUrl = audioUrl.replace("http", "https");
-                console.log("Will try to use https instead of http " + audioUrl);
+      $http.get(url, headerOptions).then(
+        function(response) {
+          console.log("Received success from url " + url);
+            // First function handles success
+            body = response.data['posts'];
+            var ret = body[Math.floor(Math.random() * body.length)];
+            $scope.randomhunt = ret;
+            var hasAudio = ret.thumbnail.media_type == 'audio';
+            if (hasAudio) {
+                var audioUrl = ret.thumbnail.metadata.url;
+                console.log("Audio meta detected " + audioUrl);
+                if (audioUrl.startsWith("http:")) {
+                    audioUrl = audioUrl.replace("http", "https");
+                    console.log("Will try to use https instead of http " + audioUrl);
+                }
+            }
+            $scope.randomhunt.hasAudio = hasAudio;
+            $scope.randomhunt.audioUrl = audioUrl;
+            if (null != $done) {
+                console.log("Calling callback done()")
+                $done();
+            }
+        },
+        function(response) {
+            //Second function handles error
+            console.log("Received error from url " + url + " with error:" +  response.statustext);
+            if (null != $done) {
+                console.log("Calling callback done()")
+                $done();
             }
         }
-        $scope.randomhunt.hasAudio = hasAudio;
-        $scope.randomhunt.audioUrl = audioUrl;
-    },
-    function(response) {
-        //Second function handles error
-        console.log("Received error from url " + url + " with error:" +  response.statustext);
-    }
-  );
+      );
+  }
 
   $scope.loadsr = function() {
     console.log("Swipe right detected");
@@ -53,6 +65,7 @@ module.controller('homeController', function($scope, $http, $localStorage, $sess
 
   ons.ready(function() {
     console.log("homeController ready");
+    $scope.refreshHunt();
   });
 
 });
