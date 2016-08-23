66f6d06a64734b9ce9a1cb51eaa53a39d5d6fa80
Bhupinder Garg <bhupinder.garg.js@snapdeal.com>
2016-08-23 21:19:58 +0530

    Formatting fixes. Added favs settings page. Added profile settings page.

--- www/js/welcomeController.js
+++ www/js/welcomeController.js
@@ -1,10 +1,12 @@
 module.controller('welcomeController', function($scope) {
 
     console.log("WelcomeController loaded");
-    if ($scope.$storage.username != null) {
-        console.log("Skipping welcome, since found stored username " + $scope.$storage.username);
+    var configure = ($scope.nav.topPage.data != null && $scope.nav.topPage.data.configure == true) || $scope.$storage.username == null;
+    $scope.configure = configure;
+    if (!configure) {
+        console.log("Skipping welcome configuration");
         setTimeout(function() {
             $scope.nav.pushPage('home.html');
         }, 2000);
-    }
+	}
 });
