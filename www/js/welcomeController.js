d84fd621f11b304706a07d98cb0df46e0eafa648
Bhupinder Garg <bhupinder.garg.js@snapdeal.com>
2016-08-23 23:45:01 +0530

    Experiments with md-card.

--- www/js/welcomeController.js
+++ www/js/welcomeController.js
@@ -3,6 +3,7 @@ module.controller('welcomeController', function($scope) {
     console.log("WelcomeController loaded");
     var configure = ($scope.nav.topPage.data != null && $scope.nav.topPage.data.configure == true) || $scope.$storage.username == null;
     $scope.configure = configure;
+    $scope.username = $scope.$storage.username;
     if (!configure) {
         console.log("Skipping welcome configuration");
         setTimeout(function() {
